664d3d9adccbdfc35ab9051516037da1ad4c7093
Irungaray <lionelirungaray@gmail.com>
2021-01-23 07:49:14 -0300

    Cyber Notebook

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import Typical from "react-typical";
 import Switch from "react-switch";
 
-import cyberHeader from '../assets/header.png';
+import cyberHeader from "../assets/header.png";
+
+import "../scss/cybr-btn.scss";
 
 class Header extends Component {
   constructor() {
@@ -31,85 +33,105 @@ class Header extends Component {
     }
     return (
       <>
-      <img
-        src={cyberHeader}
-        alt="Cyber Header"
-        className="Cyber"
-        style={{
-          top: "0",
-          bottom:"0",
-          width: "100%",
-          height: "30px",
-            objectFit: "cover"
-        }}
-      />
+        <img
+          src={cyberHeader}
+          alt="Cyber Header"
+          className="Cyber"
+          style={{
+            top: "0",
+            bottom: "0",
+            width: "100%",
+            height: "30px",
+            objectFit: "cover",
+          }}
+        />
 
-      <header
-        id="home"
-        style={{
-          // height: window.innerHeight - 140,
-          height: "87vh",
-          display: 'block'
-        }}
-      >
-        <div className="row aligner" style={{height: '100%'}}>
-          <div className="col-md-12">
-            <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-
-              <br/>
-
-              <h1 className="mb-0">
-                <Typical steps={[name]} wrapper="p" />
-              </h1>
-
-              <Typical className="title-styles" steps={[title]} wrapper="p" />
-
-              <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#212529"
-                onColor="#212529"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
+        <header
+          id="home"
+          style={{
+            // height: window.innerHeight - 140,
+            height: "87vh",
+            display: "block",
+          }}
+        >
+          <div className="row aligner" style={{ height: "100%" }}>
+            <div className="col-md-12">
+              <div>
+                {/* Cyber Notebook */}
+                <div class="cybr-btn">
                   <span
-                    className="iconify"
-                    data-icon="noto-v1:fire"
+                    className="iconify header-icon"
+                    data-icon="la:laptop-code"
                     data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
                   ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:new-moon"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              />
+                  <span></span>
+                  <span class="cybr-btn__glitch">
+                    <span
+                      className="iconify header-icon"
+                      data-icon="la:laptop-code"
+                      data-inline="false"
+                    ></span>
+                  </span>
+                  <span class="cybr-btn__tag">R25</span>
+                </div>
+
+                <br />
+
+                <div class="cybr-btn">
+                  Glitch<span>_</span>
+                  <span class="cybr-btn__glitch">Glitch_</span>
+                </div>
+
+                <h1 className="mb-0">
+                  <Typical steps={[name]} wrapper="p" />
+                </h1>
+
+                <Typical className="title-styles" steps={[title]} wrapper="p" />
+
+                <Switch
+                  checked={this.state.checked}
+                  onChange={this.onThemeSwitchChange}
+                  offColor="#212529"
+                  onColor="#212529"
+                  className="react-switch mx-auto"
+                  width={90}
+                  height={40}
+                  uncheckedIcon={
+                    <span
+                      className="iconify"
+                      data-icon="noto-v1:fire"
+                      data-inline="false"
+                      style={{
+                        display: "block",
+                        height: "100%",
+                        fontSize: 25,
+                        textAlign: "end",
+                        marginLeft: "20px",
+                        color: "#353239",
+                      }}
+                    ></span>
+                  }
+                  checkedIcon={
+                    <span
+                      className="iconify"
+                      data-icon="noto-v1:new-moon"
+                      data-inline="false"
+                      style={{
+                        display: "block",
+                        height: "100%",
+                        fontSize: 25,
+                        textAlign: "end",
+                        marginLeft: "10px",
+                        color: "#353239",
+                      }}
+                    ></span>
+                  }
+                  id="icon-switch"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </header>
-
+        </header>
       </>
     );
   }
