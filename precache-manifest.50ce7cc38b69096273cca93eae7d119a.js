4526babc844e4b27e4926dbd84b849cf6c8fccd0
Irungaray <lionelirungaray@gmail.com>
2021-01-23 17:37:54 -0300

    Updates

---
+++ precache-manifest.50ce7cc38b69096273cca93eae7d119a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7491c71ac6af78a560bddb18b60f5dca",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9923172f07f0484f34ad",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "696a32f9d4c9d25a5c2d",
+    "url": "/Portfolio/static/css/main.3a841dd1.chunk.css"
+  },
+  {
+    "revision": "9923172f07f0484f34ad",
+    "url": "/Portfolio/static/js/2.3d40797e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.3d40797e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696a32f9d4c9d25a5c2d",
+    "url": "/Portfolio/static/js/main.78172a2f.chunk.js"
+  },
+  {
+    "revision": "fd11c46505104759cf2b",
+    "url": "/Portfolio/static/js/runtime-main.b9da9406.js"
+  },
+  {
+    "revision": "7907934987a67e1327861b913b9a1a0f",
+    "url": "/Portfolio/static/media/favicon.79079349.ico"
+  },
+  {
+    "revision": "03a75ab25d8ab83c5468ad55d7e7c36b",
+    "url": "/Portfolio/static/media/footer.03a75ab2.png"
+  },
+  {
+    "revision": "62f019c8156ba186b135179377cadab5",
+    "url": "/Portfolio/static/media/footerFooter.62f019c8.png"
+  },
+  {
+    "revision": "5cc5dee188f35cd33e549b3b6dab24e9",
+    "url": "/Portfolio/static/media/footerFooterUD.5cc5dee1.png"
+  },
+  {
+    "revision": "db365ee490b23745341c7470a04ef5b3",
+    "url": "/Portfolio/static/media/header.db365ee4.png"
+  },
+  {
+    "revision": "884ab6a2b7a29db1aa6e273147f7d40f",
+    "url": "/Portfolio/static/media/headerUD.884ab6a2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d673a68edd21b67cde8c54de48c0adbf.js"
+  "/Portfolio/precache-manifest.50ce7cc38b69096273cca93eae7d119a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
