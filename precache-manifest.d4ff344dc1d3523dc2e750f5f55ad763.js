eb7dff215fa15da6e81936f4f09a016b75d99a37
Irungaray <lionelirungaray@gmail.com>
2021-01-23 16:57:44 -0300

    Updates

---
+++ precache-manifest.d4ff344dc1d3523dc2e750f5f55ad763.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a0c3a519346c1e48f77580d768cc95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9923172f07f0484f34ad",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ca19bab7495ae6678803",
+    "url": "/static/css/main.3a841dd1.chunk.css"
+  },
+  {
+    "revision": "9923172f07f0484f34ad",
+    "url": "/static/js/2.3d40797e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.3d40797e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca19bab7495ae6678803",
+    "url": "/static/js/main.7931b283.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "7907934987a67e1327861b913b9a1a0f",
+    "url": "/static/media/favicon.79079349.ico"
+  },
+  {
+    "revision": "03a75ab25d8ab83c5468ad55d7e7c36b",
+    "url": "/static/media/footer.03a75ab2.png"
+  },
+  {
+    "revision": "62f019c8156ba186b135179377cadab5",
+    "url": "/static/media/footerFooter.62f019c8.png"
+  },
+  {
+    "revision": "5cc5dee188f35cd33e549b3b6dab24e9",
+    "url": "/static/media/footerFooterUD.5cc5dee1.png"
+  },
+  {
+    "revision": "db365ee490b23745341c7470a04ef5b3",
+    "url": "/static/media/header.db365ee4.png"
+  },
+  {
+    "revision": "884ab6a2b7a29db1aa6e273147f7d40f",
+    "url": "/static/media/headerUD.884ab6a2.png"
+  }
+]);
