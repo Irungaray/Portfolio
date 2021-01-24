79194c5c661f5ff622d01f03ea9d6920c95b4530
Irungaray <lionelirungaray@gmail.com>
2021-01-24 11:24:13 -0300

    Finished Polaroid Modal

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
 
-import PolaroidModal from './PolaroidModal';
+import PolaroidModal from "./PolaroidModal";
 
 import jsIcon from "@iconify/icons-logos/javascript";
 import reactIcon from "@iconify/icons-logos/react";
@@ -32,90 +32,93 @@ class About extends Component {
       var techs = this.props.resumeBasicInfo.stack;
       var interests = this.props.resumeBasicInfo.passion;
       var trayectory = this.props.resumeBasicInfo.more;
+      var otherStuff = this.props.resumeBasicInfo.notCode;
     }
 
     return (
       <section id="about">
         <div className="col-md-12">
-          <div className="cybr-btn">
-            <h1 className="section-title" style={{ color: "black" }}><span>{sectionName}</span></h1>
-            <span className="cybr-btn__glitch"><h1 className="section-title"><span>{sectionName}</span></h1></span>
-          </div>
+          <h1 className="section-title" style={{ color: "black" }}>
+            <span>{sectionName}</span>
+          </h1>
+        </div>
 
-          <div className="row center mx-auto mb-5">
-            <div className="col-md-4 mb-5 center">
-              <div onClick={() => polaroidModalShow()}>
-                <div className="polaroid">
-                  <span style={{ cursor: "auto" }}>
-                    <img
-                      height="250px"
-                      src={profilepic}
-                      alt="Avatar placeholder"
-                    />
-                    <Icon
-                      icon={jsIcon}
-                      style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                    />
-                    <Icon
-                      icon={reactIcon}
-                      style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                    />
-                  </span>
-                </div>
+        <div className="row center mx-auto mb-5">
+          {/* aca */}
+          <div className="col-md-4 mb-5 center">
+            <div onClick={() => polaroidModalShow(otherStuff)}>
+              <div className="polaroid">
+                <span style={{ cursor: "auto" }}>
+                  <img
+                    height="250px"
+                    src={profilepic}
+                    alt="Avatar placeholder"
+                  />
+                  <Icon
+                    icon={jsIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={reactIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                </span>
               </div>
             </div>
+          </div>
+          {/* aca */}
 
-            <div className="col-md-8 center">
-              <div className="col-md-9">
-                <div className="card">
-                  <div className="card-header">
-                    <span
-                      className="iconify"
-                      data-icon="emojione:red-circle"
-                      data-inline="false"
-                    ></span>{" "}
-                    &nbsp;{" "}
-                    <span
-                      className="iconify"
-                      data-icon="twemoji:yellow-circle"
-                      data-inline="false"
-                    ></span>{" "}
-                    &nbsp;{" "}
-                    <span
-                      className="iconify"
-                      data-icon="twemoji:green-circle"
-                      data-inline="false"
-                    ></span>
-                  </div>
-                  <div
-                    className="card-body font-trebuchet text-justify ml-3 mr-3"
-                    style={{
-                      height: "auto",
-                      fontSize: "132%",
-                      lineHeight: "200%",
-                    }}
-                  >
+          <div className="col-md-8 center">
+            <div className="col-md-9">
+              <div className="card">
+                <div className="card-header">
+                  <span
+                    className="iconify"
+                    data-icon="emojione:red-circle"
+                    data-inline="false"
+                  ></span>{" "}
+                  &nbsp;{" "}
+                  <span
+                    className="iconify"
+                    data-icon="twemoji:yellow-circle"
+                    data-inline="false"
+                  ></span>{" "}
+                  &nbsp;{" "}
+                  <span
+                    className="iconify"
+                    data-icon="twemoji:green-circle"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div
+                  className="card-body font-trebuchet text-justify ml-3 mr-3"
+                  style={{
+                    height: "auto",
+                    fontSize: "132%",
+                    lineHeight: "200%",
+                  }}
+                >
+                  <br />
 
+                  <PolaroidModal
+                    show={this.state.polaroidModalShow}
+                    onHide={polaroidModalClose}
+                    data={this.state.deps}
+                  />
+
+                  <div className="about">
+                    <span className="wave" role="img" aria-label="">
+                      {hello} 😎{" "}
+                    </span>
                     <br />
-
-                    <PolaroidModal
-                      show={this.state.polaroidModalShow}
-                      onHide={polaroidModalClose}
-                      data={this.state.deps}
-                    />
-
-                    <div className="about">
-                      <span className="wave" role="img" aria-label="">{hello} 😎 </span>
-                      <br />
-                      <br />
-                      <h3>{name}</h3>
-                      <br />
-                      <h3>{techs}</h3>
-                      <br />
-                      <h3>{interests}</h3>
-                      <br />
-                      <h3>{trayectory} LINK</h3>
-                    </div>
+                    <br />
+                    <h3>{name}</h3>
+                    <br />
+                    <h3>{techs}</h3>
+                    <br />
+                    <h3>{interests}</h3>
+                    <br />
+                    <h3>{trayectory} LINK</h3>
                   </div>
                 </div>
               </div>
--- src/components/PolaroidModal.js
+++ src/components/PolaroidModal.js
@@ -14,59 +14,62 @@ import polaroid4 from "../assets/polaroidModal/polaroid4.jpg"
 
 class PolaroidModal extends Component {
   render() {
+    if (this.props.data) {
+      var otherStuff = this.props.data;
+    }
     return (
-      <Modal
-        {...this.props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        className="modal-inside"
-      >
-        <span onClick={this.props.onHide} className="modal-close">
-          <i className="fas fa-times fa-3x close-icon"></i>
-        </span>
-        <div className="col-md-12">
-          <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
-            <div className="slider-tab">
-              <span
-                className="iconify slider-iconfiy"
-                data-icon="emojione:red-circle"
-                data-inline="false"
-                style={{ marginLeft: "5px" }}
-              ></span>{" "}
-              &nbsp;{" "}
-              <span
-                className="iconify slider-iconfiy"
-                data-icon="twemoji:yellow-circle"
-                data-inline="false"
-              ></span>{" "}
-              &nbsp;{" "}
-              <span
-                className="iconify slider-iconfiy"
-                data-icon="twemoji:green-circle"
-                data-inline="false"
-              ></span>
+      <div className="polaroid-modal">
+          <Modal
+          {...this.props}
+          size="xl"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          className="modal-inside"
+          >
+          <span onClick={this.props.onHide} className="modal-close">
+            <i className="fas fa-times fa-3x close-icon"></i>
+          </span>
+          <div className="col-md-12">
+            <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
+              <div className="slider-tab">
+                <span
+                  className="iconify slider-iconfiy"
+                  data-icon="emojione:red-circle"
+                  data-inline="false"
+                  style={{ marginLeft: "5px" }}
+                ></span>{" "}
+                &nbsp;{" "}
+                <span
+                  className="iconify slider-iconfiy"
+                  data-icon="twemoji:yellow-circle"
+                  data-inline="false"
+                ></span>{" "}
+                &nbsp;{" "}
+                <span
+                  className="iconify slider-iconfiy"
+                  data-icon="twemoji:green-circle"
+                  data-inline="false"
+                ></span>
+              </div>
+              <AwesomeSlider
+                animation="scaleOutAnimation"
+                className="slider-image"
+              >
+                <div> <img src={polaroid} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
+                <div> <img src={polaroid1} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
+                <div> <img src={polaroid2} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
+                <div> <img src={polaroid3} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
+                <div> <img src={polaroid4} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
+                {/* {imgMap} */}
+            </AwesomeSlider>
             </div>
-            <AwesomeSlider
-              animation="scaleOutAnimation"
-              className="slider-image"
-            >
-              <div> <img src={polaroid} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
-              <div> <img src={polaroid1} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
-              <div> <img src={polaroid2} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
-              <div> <img src={polaroid3} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
-              <div> <img src={polaroid4} alt="Me, myself and I" style={{ maxHeight: "399px"}}/> <br /> <h1>BEEP BEEP MOTHERFUCKER!</h1> </div>
-              {/* {imgMap} */}
-           </AwesomeSlider>
-          </div>
 
-          <div className="col-md-12 text-center">
-            {/* <h1 style={{ padding: "5px 5px 0 5px" }}>{title}</h1> */}
-
-            {/* <h2 className="modal-description">{description}</h2> */}
+            <div className="col-md-12 text-center">
+              <h1 style={{ paddingBottom: "15px" }}>{otherStuff}</h1>
+            </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
+      </div>
     );
   }
 }
