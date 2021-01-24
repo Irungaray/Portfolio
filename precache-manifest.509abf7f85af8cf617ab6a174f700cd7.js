9804053e5c0328b04d201a039be4522d12cdfefe
Irungaray <lionelirungaray@gmail.com>
2021-01-24 14:39:32 -0300

    Updates

---
+++ precache-manifest.509abf7f85af8cf617ab6a174f700cd7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901d27c5f799190dbe82f41d66644066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5da0e17dbdd3b7c9c71e",
+    "url": "/static/css/2.8573ad47.chunk.css"
+  },
+  {
+    "revision": "44af9fa03723a0c8da6b",
+    "url": "/static/css/main.a8519a75.chunk.css"
+  },
+  {
+    "revision": "5da0e17dbdd3b7c9c71e",
+    "url": "/static/js/2.90ef8e12.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.90ef8e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44af9fa03723a0c8da6b",
+    "url": "/static/js/main.0323b575.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "7907934987a67e1327861b913b9a1a0f",
+    "url": "/static/media/favicon.79079349.ico"
+  },
+  {
+    "revision": "03a75ab25d8ab83c5468ad55d7e7c36b",
+    "url": "/static/media/footer.03a75ab2.png"
+  },
+  {
+    "revision": "62f019c8156ba186b135179377cadab5",
+    "url": "/static/media/footerFooter.62f019c8.png"
+  },
+  {
+    "revision": "5cc5dee188f35cd33e549b3b6dab24e9",
+    "url": "/static/media/footerFooterUD.5cc5dee1.png"
+  },
+  {
+    "revision": "db365ee490b23745341c7470a04ef5b3",
+    "url": "/static/media/header.db365ee4.png"
+  },
+  {
+    "revision": "884ab6a2b7a29db1aa6e273147f7d40f",
+    "url": "/static/media/headerUD.884ab6a2.png"
+  },
+  {
+    "revision": "a2e7aed4d3ccb3620f152c9f653ba472",
+    "url": "/static/media/polaroid.a2e7aed4.jpg"
+  },
+  {
+    "revision": "b5cb36415ed88872f74ef14e2924b544",
+    "url": "/static/media/polaroid1.b5cb3641.png"
+  },
+  {
+    "revision": "2d5199479a3e28ed9b1c216774a16718",
+    "url": "/static/media/polaroid2.2d519947.jpg"
+  },
+  {
+    "revision": "10c4c6269e58e71506fc141035da9b5d",
+    "url": "/static/media/polaroid3.10c4c626.jpg"
+  },
+  {
+    "revision": "91190cb36e4c2bfa48278d9b12e38c26",
+    "url": "/static/media/polaroid4.91190cb3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.50ce7cc38b69096273cca93eae7d119a.js"
+  "/precache-manifest.509abf7f85af8cf617ab6a174f700cd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
